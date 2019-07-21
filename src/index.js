12b6976d7cbc845379d6db9124c13843041116c8
MadhuriAlla <madhuribhimereddy@gmail.com>
2019-07-21 20:36:18 +0530

    Updated the index file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 
 //Create a react component
 const App =() => {
-    return <div>Hello Good Morning.!! How are you.?</div>
+    return <div>Hello Good Morning.!! Welcome.!!.?</div>
 }
 
 
